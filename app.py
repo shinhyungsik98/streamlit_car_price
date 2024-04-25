e5a86c8100d29882bdc719c8e1027c1bfaf9787a
shinhyungsik98 <ragi08@naver.com>
2024-04-25 11:38:32 +0900

    버전 1.0

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from home import run_home
 from eda import run_eda
+from ml import run_ml
 
 def main() :
     st.title('자동차 가격 예측 앱')
@@ -14,7 +15,7 @@ def main() :
     elif choice == menu[1] :
         run_eda()
     elif choice == menu[2] :
-        pass
+        run_ml()
 
 
 if __name__ == '__main__' :
